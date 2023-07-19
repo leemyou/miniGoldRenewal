acb53ef447d82de39212dc0a97762930654a63ef
Emyo <84968044+leemyou@users.noreply.github.com>
2023-07-19 17:40:27 +0900

    FEAT: 메인배너 swiper 기능구현 중

---
+++ js/mainBanner.js
@@ -0,0 +1,37 @@
+
+// window.onload = function () {
+//     // $('.front-text')[0].innerHTML = '바보';
+
+// }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true, // infinity
+    spaceBetween: 30,
+
+    autoplay: {
+        delay: 5000,
+    },
+  
+    // pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    },
+  
+  });
+
+  window.onload = function () {
+    $('.test')[0].innerHTML = '123';
+
+    console.log("hi");
+    
+
+}
